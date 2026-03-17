bb27730ac087388ea7d0867cd4718e7ef130e9a5
Miguel Angel Peralta Martinez <106187870+MiguelPM01@users.noreply.github.com>
2026-03-17 17:50:18 -0600

    Update conditional check from 50 to 10

--- 01-fundamentos-programacion/ejercicios/03-condicional.js
+++ 01-fundamentos-programacion/ejercicios/03-condicional.js
@@ -1,6 +1,8 @@
 let numero = 77;
 
-if (numero > 50) {
-    console.log(`El número ${numero} es mayor que 50`);
+if (numero > 10) {
+    console.log(`El número ${numero} es mayor que 10`);
+} else {
+    console.log(`El número ${numero} es menor o igual a 10`);
 }
 
